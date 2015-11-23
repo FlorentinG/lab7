33c57e779dc1f28c1993df4d0709d8ba3dc18890
davidweicker <david.weicker@student.uclouvain.be>
2015-11-23 18:37:55 +0100

    yolo

--- lsp.m
+++ lsp.m
@@ -1,5 +1,9 @@
 function [res] = lsp()
-%LSP 
+%LSP Solves the least squares problem with the basis functions given in 
+% the assignment.
+% res = norm of the residual for n=1:3
+%
+% Authors : Weicker David & Goyens Florentin
 close all;
 
 mauna = load('mauna.dat');
@@ -19,6 +23,7 @@ for n=1:3
     res(n) = norm(A*x-b);
 end
 
+%Plot for n=3
 plot(t,b,'bx',t,A*x,'r-');title('LSQ with QR-factorization');xlabel('Time (months)');
 ylabel('CO2 concentration (ppm)');
 
