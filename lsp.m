250d44761ca1189ed2639db5957ebab980650eb6
davidweicker <david.weicker@student.uclouvain.be>
2015-11-23 19:41:19 +0100

    partB

--- lsp.m
+++ lsp.m
@@ -1,7 +1,8 @@
-function [res] = lsp()
+function [res,param] = lsp()
 %LSP Solves the least squares problem with the basis functions given in 
 % the assignment.
 % res = norm of the residual for n=1:3
+% param = contains the values of the parameters a_i,b_i,c_i
 %
 % Authors : Weicker David & Goyens Florentin
 close all;
@@ -15,12 +16,14 @@ t = (1:m)';
 alpha = 0.00037;
 e = ones(m,1);
 res = zeros(3,1);
+param = zeros(8,3);
 
 for n=1:3
     A = [e exp(alpha*t) cos(2*pi*t*(1:n)/12) sin(2*pi*t*(1:n)/12)];
     [q,r] = qr(A);
     x = r\(q'*b);
     res(n) = norm(A*x-b);
+    param(1:2+2*n,n) = x;
 end
 
 %Plot for n=3
